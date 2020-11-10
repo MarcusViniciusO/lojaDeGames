4cfc54439c9889427446eca17306cb1009488ce4
Marcus Vinicius <marcusviniciuso@outlook.com.br>
2020-11-10 17:26:57 -0300

    ProjetoLoja

---
+++ games/src/main/java/com/lojadejogos/games/model/Categoria.java
@@ -0,0 +1,67 @@
+package com.lojadejogos.games.model;
+
+
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max= 100)
+	private String genero;
+	
+	@NotNull
+	@Size(min = 1, max= 50)
+	private String origem;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date (System.currentTimeMillis());
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(String origem) {
+		this.origem = origem;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+}
