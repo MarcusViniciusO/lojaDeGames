261256c44aedb7fd08970e2a45450170679ed047
Marcus Vinicius <marcusviniciuso@outlook.com.br>
2020-11-10 18:47:19 -0300

    Versão 1.6

--- games/src/main/java/com/lojadejogos/games/repository/CategoriaRepository.java
+++ games/src/main/java/com/lojadejogos/games/repository/CategoriaRepository.java
@@ -2,11 +2,11 @@ package com.lojadejogos.games.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import com.lojadejogos.games.model.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	public List<Categoria> findAllByGeneroContainingIgnoreCase (String genero);
-
 }
